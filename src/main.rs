e6cb8408b2d59fc8169f169718a546d1c0dacb55
Fernando Villalba <43227912+falcondpr@users.noreply.github.com>
2025-07-12 19:32:04 +0000

    feat: hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+#![allow(unused)]
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut name: String = String::new();
+    let greeting: &str = "Nice to meet you";
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Didn't receive Input");
+    
+    println!("Hello {}! {}", name.trim_end(), greeting);
 }
+
+/*
+    // === Hello world! === // 
+    fn main() {
+        let mut name: String = String::new();
+        let greeting: &str = "Nice to meet you";
+        io::stdin()
+            .read_line(&mut name)
+            .expect("Didn't receive Input");
+
+        println!("Hello {}! {}", name.trim_end(), greeting);
+    }
+*/
